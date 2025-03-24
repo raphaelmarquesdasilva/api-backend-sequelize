7d22e0260ffa75444490e1da544aee35230439e9
Raphaelzin <raphael_thcr@hotmail.com>
2025-03-23 22:08:35 -0300

    User's table created sucessfully.

---
+++ Models/user.js
@@ -0,0 +1,35 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../Databases/database.js";
+
+
+const User = sequelize.define("User", {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    fullName: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    birthDate: {
+        type: DataTypes.DATEONLY,
+        allowNull: false
+    }
+})
+
+export default User
--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express"
 import cors from "cors"
 import sequelize from "./Databases/database.js"
+import User from "./Models/user.js"
 
 const app = express()
 const PORT = process.env.BACK_PORT
@@ -16,6 +17,7 @@ app.use(cors())
 const testingConnection = async () => {
     try {
         await sequelize.authenticate()
+        await User.sync({force: false})
         console.log(`Database connection has been stablished successfully!`)
         app.listen(PORT, () => {
             console.log(`Server listening on http://localhost:${PORT}`)
